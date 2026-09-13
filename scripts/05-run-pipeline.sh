38ff07af1e0e7ae104c090a22017f6206a00bdb3
Hector Jojoa <ds.hectorjojoa@gmail.com>
2026-09-13 11:22:23 -0500

    fix: reemplazar ClusterTask git-clone obsoleto por Task propia

--- scripts/05-run-pipeline.sh
+++ scripts/05-run-pipeline.sh
@@ -2,6 +2,7 @@
 # Dispara manualmente los pipelines de Tekton (build + push + actualizar manifest)
 set -e
 kubectl apply -f ../tekton/tasks/task-git-clone.yaml
+kubectl apply -f ../tekton/tasks/task-git-clone.yaml
 kubectl apply -f ../tekton/tasks/task-build-push.yaml
 kubectl apply -f ../tekton/tasks/task-update-manifest.yaml
 kubectl apply -f ../tekton/pipeline-backend.yaml
